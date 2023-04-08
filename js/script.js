edea630d795b1b977523999d190979f555dc5d5f
Agnieszka Ciesińska <agnieszkaciesinska@gmail.com>
2023-04-08 20:08:47 +0200

    Add first js variables computerMove and playerMove

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, PlayerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+PlayerMove = 'papier';
+printMessage('Zagrałem ' + PlayerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
